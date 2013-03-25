956a0aa2f63b0efd3dec942b74cd433f8645bd66
Justin McAdams <mcadamsrails@gmail.com>
2013-03-25 11:51:04 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleAppprc::Application.config.secret_token = 'db5edca929359e4d10e2bb5860aeaf95ff48ebc89c535086e492a2ed040d357a358b85f11cd799ffedebe630a8affe2f71df1d68c060d1e16c051410d5bd8b4a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SampleAppprc::Application.config.session_store :cookie_store, key: '_sample_appprc_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SampleAppprc::Application.config.session_store :active_record_store
